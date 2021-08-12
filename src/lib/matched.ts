751e60740ea8e5688c21f771bdb2ddd66bc5d376
Kousha Talebian <kousha.talebian@gmail.com>
2021-08-12 10:33:14 -0700

    fix security vulnerabilities

--- src/lib/matched.ts
+++ src/lib/matched.ts
@@ -5,6 +5,7 @@ import { Matched } from './switch';
  *
  * @param value		the value to return
  */
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function doReturn<R>(value: any): Matched<R, any> {
   return () => {
     return value;
